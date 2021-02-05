36176983bf43be508451152f28b1be8919175ba3
MsanEsan26 <75089450+MsanEsan26@users.noreply.github.com>
2021-02-05 14:07:34 +0300

    Add files via upload

---
+++ sunucuAyar.js
@@ -0,0 +1,13 @@
+const ayarlar = {
+  guildID: '804311060982005790', // sunucu idsi
+  ownerRole:"804311061200502786", // sunucu sahiplerinin rolü
+  teyitsizRolleri:[804311061091188736], // kayıt olmayanlara verieln rol
+  jailRolu:"804311061154234374", // cezalılara verilen rol
+  enAltYetkiliRolu:[804311061138374713], // ilk yetkili permi
+  activity:"Phobia 💖", // botun durumu
+  status:"idle",
+  invitelink:"https://discord.gg/SSHU6rzVYA", //davet linki
+  symbol:"⚝ ", // tag
+  voicechannel:"805390873973489714" // botun gireceği ses kanalı
+};
+module.exports = ayarlar;
